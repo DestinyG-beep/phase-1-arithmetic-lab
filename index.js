b18bf44b6fe37b9c7179fba192a797ba200ffc96
mburugudestinygakenga <mburugugakenga@gmail.com>
2024-10-21 02:37:37 +0300

    edits index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 24; 
+const num4 = 20; 
+const mod = num3 % num4; 
+
+const numbers = [10, 15, 20, 5];
+const max = Math.max(...numbers);
+
+console.log("Multiply:", multiply);
+console.log("Random:", random);
+console.log("Mod:", mod);
+console.log("Max:", max);
